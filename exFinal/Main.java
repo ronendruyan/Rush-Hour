33fb97d3e45806a41df40586f24d0b06dcdfe1c0
Matan Nahum <137485536+Matan7050@users.noreply.github.com>
2024-06-25 15:49:58 +0300

    l2
    l2

---
+++ exFinal/Main.java
@@ -0,0 +1,11 @@
+package exFinal;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+            new gameBoard().setVisible(true);
+        });
+    }
+}
