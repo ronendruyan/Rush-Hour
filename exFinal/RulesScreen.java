807821ed92b3b73f843487fc445f2cf4a75915a3
Zivossi <157590304+Zivossi@users.noreply.github.com>
2024-08-18 13:34:05 +0300

    Update RulesScreen.java

--- exFinal/RulesScreen.java
+++ exFinal/RulesScreen.java
@@ -1,27 +1,58 @@
 package exFinal;
 
 import java.awt.Font;
-
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-//class to draw the rules screem
-//TODO add rules for the game and improve graphics
+// Class to draw the rules screen
+// TODO add rules for the game and improve graphics
 public class RulesScreen extends Screen {
 
-	public RulesScreen(ScreenManager screenManager) {
-		super();
-		System.out.println("rulesScreen");
-		String text = new String("this is how you play");
-		JLabel label = new JLabel(text, SwingConstants.CENTER);
-		label.setFont(new Font("Serif", Font.PLAIN, 24));
-		label.setBounds(100, 100, 300, 100);
-		ButtonComp back = new ButtonComp("menu_button");
-		back.setBounds(300, 300, 100, 50);
-		back.setAction(new Action("toMenu", 0), screenManager);
-		_components.add(label);
-		_components.add(back.getButton());
+    public RulesScreen(ScreenManager screenManager) {
+        super();
 
-	}
+        // Text for labels, using HTML tags for multiline support and bold titles
+        String text1 = "<html><b>Welcome to Rush Hour!</b><br><br>" +
+                       "<b>Objective:</b><br>" +
+                       "Your goal is to move the red car out of the parking lot by shifting other vehicles blocking its path.<br>" +
+                       "You need to get the red car to the exit on the right side of the grid.<br><br></html>";
 
+        String text2 = "<html><b>How to Play:</b><br>" +
+                       "1. Use the mouse to select and drag the vehicles.<br>" +
+                       "2. Vehicles can only move forward or backward along the grid in the direction they are facing.<br>" +
+                       "3. You cannot move vehicles diagonally or into occupied spaces.<br>" +
+                       "4. The game tracks the number of moves you make. Try to solve the puzzle with the fewest moves possible to achieve a higher score.<br><br></html>";
+
+        String text3 = "<html><b>Scoring:</b><br>" +
+                       "1. Achieve the optimal number of moves for a perfect score of 10.<br>" +
+                       "2. Your score decreases as the number of moves increases beyond the optimal solution.<br>" +
+                       "3. The closer your moves are to the optimal number, the higher your score.<br><br>" +
+                       "Good luck, and enjoy the challenge!</html>";
+
+        // Create labels for each section using the desired format
+        JLabel label1 = new JLabel(text1, SwingConstants.CENTER);
+        JLabel label2 = new JLabel(text2, SwingConstants.CENTER);
+        JLabel label3 = new JLabel(text3, SwingConstants.CENTER);
+
+        // Set font and bounds for each label
+        label1.setFont(new Font("Serif", Font.PLAIN, 18));
+        label1.setBounds(50, 20, 500, 100);
+
+        label2.setFont(new Font("Serif", Font.PLAIN, 18));
+        label2.setBounds(50, 130, 500, 150);
+
+        label3.setFont(new Font("Serif", Font.PLAIN, 18));
+        label3.setBounds(50, 290, 500, 150);
+
+        // Back button
+        ButtonComp back = new ButtonComp("menu_button");
+        back.setBounds(300, 450, 100, 50);
+        back.setAction(new Action("toMenu", 0), screenManager);
+
+        // Add labels and button to the screen
+        _components.add(label1);
+        _components.add(label2);
+        _components.add(label3);
+        _components.add(back.getButton());
+    }
 }
